5368f3df907a515c946552003b96380352d73a36
alex-sdn <alex@DESKTOP-2A3OSDH.localdomain>
2023-08-29 16:08:42 +0200

    add Channel class

---
+++ include/Channel.hpp
@@ -0,0 +1,51 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+# include <string>
+# include <vector>
+
+class Channel {
+
+	private :
+		std::string		_name;
+		std::string		_topic;
+		bool			_inviteOnly;  	// -i
+		bool			_topicRights; 	// -t
+		std::string		_password;    	// -k
+		int				_userLimit;   	// -l
+		std::vector<std::string>	_operators; // -o
+		std::vector<std::string>	_members;   //save nickname
+
+	public :
+		Channel(std::string name, std::string creator);  //envoyer password?
+		~Channel();
+
+		std::string	getName() const;  //pas de setter
+		std::string	getTopic() const;
+		void		setTopic(std::string topic);
+		bool		getInviteOnly() const;
+		void		setInviteOnly(bool mode);
+		bool		getTopicRights() const;
+		void		setTopicRights(bool mode);
+		std::string	getPassword() const;
+		void		setPassword(std::string password);
+		int			getUserLimit() const;
+		void		setUserLimit(int limit);
+
+		void		addOperator(std::string nickname);
+		void		rmOperator(std::string nickname);
+		bool		isOperator(std::string nickname) const;
+
+		void		addMember(std::string nickname);
+		void		rmMember(std::string nickname);
+		bool		isMember(std::string nickname) const;
+
+};
+
+
+// MODES
+// -i  :  bool
+// -t  :  bool
+// -k  :  string
+// -o  :  vecteur de string (membres)
+// -l  :  int (-1 if no limit)
---
+++ srcs/Channel.cpp
@@ -0,0 +1,94 @@
+#include "Channel.hpp"
+
+Channel::Channel(std::string name, std::string creator) : _name(name)
+{
+	_members.push_back(creator);
+	// verif param de bases
+	_inviteOnly = false;
+	_topicRights = false;
+	_userLimit = -1;
+}
+
+Channel::~Channel() {}
+
+std::string	Channel::getName() const { return _name; }
+
+std::string	Channel::getTopic() const { return _topic; }
+void		Channel::setTopic(std::string topic) { _topic = topic; }
+
+bool		Channel::getInviteOnly() const { return _inviteOnly; }
+void		Channel::setInviteOnly(bool mode) { _inviteOnly = mode; }
+
+bool		Channel::getTopicRights() const { return _topicRights; }
+void		Channel::setTopicRights(bool mode) { _topicRights = mode; }
+
+std::string	Channel::getPassword() const { return _password; }
+void		Channel::setPassword(std::string password) { _passwor = password; }
+
+int			Channel::getUserLimit() const { return _userLimit; }
+void		Channel::setUserLimit(int limit) { _userLimit = limit; }
+
+void		Channel::addOperator(std::string nickname)
+{
+	// case sensitive ? a verif
+	for (size_t i = 0; i < _operators.size(); i++) {
+		if (_operators[i] == nickname)
+			return ;
+	}
+	_operators.push_back(nickname);
+}
+
+void		Channel::rmOperator(std::string nickname)
+{
+	for (size_t i = 0; i < _operators.size(); i++) {
+		if (_operators[i] == nickname) {
+			_operators.erase(_operators.begin() + i);
+			return;  //pas de doublons normalement
+		}
+	}
+}
+
+bool		Channel::isOperator(std::string nickname) const
+{
+	for (size_t i = 0; i < _operators.size(); i++) {
+		if (_operators[i] == nickname)
+			return true;
+	}
+	return false;
+}
+
+void		Channel::addMember(std::string nickname)
+{
+	// case sensitive ? a verif
+	for (size_t i = 0; i < _members.size(); i++) {
+		if (_members[i] == nickname)
+			return ;
+	}
+	_members.push_back(nickname);
+}
+
+void		Channel::rmMember(std::string nickname)
+{
+	for (size_t i = 0; i < _members.size(); i++) {
+		if (_members[i] == nickname) {
+			_members.erase(_members.begin() + i);
+			break;  //pas de doublons normalement
+		}
+	}
+	// if member is also op
+	for (size_t i = 0; i < _operators.size(); i++) {
+		if (_operators[i] == nickname) {
+			_operators.erase(_operators.begin() + i);
+			return;  //pas de doublons normalement
+		}
+	}
+}
+
+bool		Channel::isMember(std::string nickname) const
+{
+	for (size_t i = 0; i < _members.size(); i++) {
+		if (_members[i] == nickname)
+			return true;
+	}
+	return false;
+}
