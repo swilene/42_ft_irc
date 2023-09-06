794611245f6f2652cde1af88c5a6e64946ac5884
Alexandre SADANOWICZ <asadanow@paul-f5Br6s2.clusters.42paris.fr>
2023-09-06 12:53:53 +0200

    add JOIN rpl (fix PART)

--- srcs/Messages.cpp
+++ srcs/Messages.cpp
@@ -103,8 +103,7 @@ void	Messages::joinMsg(Client *client, std::string msg, std::vector<Client *> cl
 	std::string name = msg.substr(msg.find("#", 0) + 1, std::string::npos);
 	name = name.substr(0, name.find("\r\n", 0));
 	size_t		i = 0;
-	
-	// not sending any RPL ...
+
 	while (i < channels.size()) {
 		if (channels[i].getName() == name)
 			break;
@@ -114,10 +113,14 @@ void	Messages::joinMsg(Client *client, std::string msg, std::vector<Client *> cl
 	{
 		Channel newchan(name, client);
 		channels.push_back(newchan);
+		_RPL = ":" + client->getNick() + "!" + client->getUser() + "@127.0.0.1" + " JOIN #" + name + "\r\n";
+		_RPLtarget.push_back(client);
 	}
 	else {
 		std::cout << "joined existing chan" << std::endl;
 		channels[i].addMember(client);
+		_RPL = ":" + client->getNick() + "!" + client->getUser() + "@127.0.0.1" + " JOIN #" + name + "\r\n";
+		_RPLtarget.push_back(client);
 	}
 }
 
