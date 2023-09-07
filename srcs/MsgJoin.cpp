be7c30a7bdc5ba3a505782af14366851a57a9b52
Alexandre SADANOWICZ <asadanow@paul-f5Br5s1.clusters.42paris.fr>
2023-09-07 12:45:14 +0200

    JOIN name list OK

--- srcs/MsgJoin.cpp
+++ srcs/MsgJoin.cpp
@@ -27,8 +27,17 @@ void	Messages::joinMsg(Client *client, std::string msg, std::vector<Client *> cl
 		_RPLtarget.push_back(client);
 		if (!channels[i].getTopic().empty())  //RPL_TOPIC
 			_RPL += ":127.0.0.1 332 " + client->getNick() + " #" + name + " :" + channels[i].getTopic() + "\r\n";
-		
 	}
 	// RPL_NAMREPLY
-	// _RPL += ":127.0.0.1 353 " + client->getNick() + " = "
+	_RPL += ":127.0.0.1 353 " + client->getNick() + " = #" + name + " :";
+	for (size_t j = 0; j < channels[i].getMembers().size(); j++) {
+		if (channels[i].isOperator(channels[i].getMembers()[j]))  //==is ope
+			_RPL += "@";
+		_RPL += channels[i].getMembers()[j]->getNick();  // c moche
+		if (j + 1 < channels[i].getMembers().size())
+			_RPL += " ";
+	}
+	_RPL += "\r\n";
+	// RPL_ENDOFNAMES
+	_RPL += ":127.0.0.1 366 " + client->getNick() + " #" + name + " :End of NAMES list\r\n";
 }
