6ee3813245325b6f5caee4b52e8a836d90c94d91
Alexandre SADANOWICZ <asadanow@paul-f5Ar2s4.clusters.42paris.fr>
2023-09-11 16:03:17 +0200

    add _realname in Client

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -19,12 +19,14 @@ Client::~Client() { close(_fd); }
 // setter
 void Client::setUser(std::string user) { _user = user; }
 void Client::setNick(std::string nick) { _nick = nick; }
+void Client::setRealname(std::string realname) { _realname = realname; }
 void Client::setHost(std::string host) { _host = host; }
 void Client::setRegistered(void)	   { _registered = true; }
 
 // getter
 std::string Client::getUser() const { return (_user); }
 std::string Client::getNick() const { return (_nick); }
+std::string Client::getRealname() const { return (_realname); }
 std::string Client::getHost() const { return (_host); }
 int 		Client::getFd() const { return(_fd); }
 bool		Client::getRegistered() const { return _registered; }
